c1e9b701195086bdcf0143d90411d977ad3f05b4
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-09 15:43:34 +0530

    Added: Creat stacks using linked list

--- StacksAndQueues.java
+++ StacksAndQueues.java
@@ -3,5 +3,13 @@ package com.StacksAndQueues;
 public class StacksAndQueues {
     public static void main(String[] args) {
         System.out.println(" Welcome to StackQueue Problem ");
+        MyStackOps stack = new MyStackOps();// UC1
+        stack.push(56);
+        stack.push(30);
+        stack.push(70);
+
+        stack.printer();
+
     }
-}
+    }
+
