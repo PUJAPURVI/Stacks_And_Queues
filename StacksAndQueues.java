65282c2671ddb49935708f3f0f208e2499325254
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-09 15:38:13 +0530

    Added: Welcome message

---
+++ StacksAndQueues.java
@@ -0,0 +1,7 @@
+package com.StacksAndQueues;
+
+public class StacksAndQueues {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to StackQueue Problem ");
+    }
+}
